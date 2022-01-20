16b57a8a5918e0ad5715d9aba650027a10f0ca81
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-21 00:01:14 +0900

    scrape then save to db

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,13 @@
-p 'TEST'
+require_relative 'KakakuScraper'
+require_relative 'MobilePhone'
+
+scraper = KakakuScraper.new()
+data = scraper.scrape
+
+data.each_with_index {|datum, i|
+  mobile_phone = MobilePhone.new()
+  mobile_phone.maker = datum[:maker]
+  mobile_phone.name = datum[:name]
+  mobile_phone.ranking = i + 1
+  mobile_phone.save
+}
