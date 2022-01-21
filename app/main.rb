32e953c5f920243203f9abb0fb2247955eb875af
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-21 22:11:39 +0900

    separate scrape logic from main

--- app/main.rb
+++ app/main.rb
@@ -1,13 +1,1 @@
-require_relative 'KakakuScraper'
-require_relative 'MobilePhone'
-
-scraper = KakakuScraper.new()
-data = scraper.scrape
-
-data.each_with_index {|datum, i|
-  mobile_phone = MobilePhone.new()
-  mobile_phone.maker = datum[:maker]
-  mobile_phone.name = datum[:name]
-  mobile_phone.ranking = i + 1
-  mobile_phone.save
-}
+puts 'main'
